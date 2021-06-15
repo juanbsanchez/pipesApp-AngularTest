211211c754cd10d6d9538bd77c944ba5134f5359
juanbsanchez <juanbsanchez1988@gmail.com>
2021-06-15 17:45:35 +0200

    add button from primeng and global styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {ButtonModule} from 'primeng/button';
 
+// PrimeNg
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
