ea7d7c40c30ed576c9dc5b1dbc9f80c0cc8feb1d
juanbsanchez <juanbsanchez1988@gmail.com>
2021-06-15 19:59:57 +0200

    exports components

--- src/app/sales/sales.module.ts
+++ src/app/sales/sales.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NumbersComponent } from './pages/numbers/numbers.component';
+import { NotCommonsComponent } from './pages/not-commons/not-commons.component';
+import { BasicsComponent } from './pages/basics/basics.component';
+import { OrderComponent } from './pages/order/order.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NumbersComponent,
+    NotCommonsComponent,
+    BasicsComponent,
+    OrderComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    NumbersComponent,
+    NotCommonsComponent,
+    BasicsComponent,
+    OrderComponent
   ]
 })
 export class SalesModule { }
