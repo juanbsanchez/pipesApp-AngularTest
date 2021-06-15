d32cc30a049edd01c5f4b2ce63d429c95dd74349
juanbsanchez <juanbsanchez1988@gmail.com>
2021-06-15 20:55:56 +0200

    import PrimeNgModule

--- src/app/sales/sales.module.ts
+++ src/app/sales/sales.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import {PrimeNgModule} from "../prime-ng/prime-ng.module";
+
+//Components
 import { NumbersComponent } from './pages/numbers/numbers.component';
 import { NotCommonsComponent } from './pages/not-commons/not-commons.component';
 import { BasicsComponent } from './pages/basics/basics.component';
 import { OrderComponent } from './pages/order/order.component';
+
 
 
 
@@ -15,7 +20,8 @@ import { OrderComponent } from './pages/order/order.component';
     OrderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    PrimeNgModule
   ],
   exports: [
     NumbersComponent,
