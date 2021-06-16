b8d361033568721a6d94c75e635e7c48e3d482b4
juanbsanchez <juanbsanchez1988@gmail.com>
2021-06-16 20:39:31 +0200

    change language of app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {SharedModule} from "./shared/shared.module";
 import {AppRouterModule} from "./app-router.module";
 import {SalesModule} from "./sales/sales.module";
 
-
+//Change locale app
+import localEs from '@angular/common/locales/es';
+import {registerLocaleData} from '@angular/common';
+registerLocaleData(localEs);
 
 
 @NgModule({
@@ -18,7 +21,9 @@ import {SalesModule} from "./sales/sales.module";
     SharedModule,
     SalesModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'es'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
