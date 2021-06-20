6cceb1cc92087fbf2425fea5dc8b8e4fabe5ceb9
juanbsanchez <juanbsanchez1988@gmail.com>
2021-06-20 10:42:57 +0200

    create custom pipe to order heroes[]

---
+++ src/app/sales/pipes/order.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {Heroe} from "../interfaces/sales.interface";
+
+@Pipe({
+  name: 'order'
+})
+export class OrderPipe implements PipeTransform {
+
+  transform(heroes: Heroe[]): Heroe[] {
+
+    heroes = heroes.sort( (a,b) => (a.name > b.name) ? 1 : -1);
+
+    return heroes;
+
+  }
+
+}
