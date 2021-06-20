6d97f8a81ce9ce99585e91d1af6b943d59086123
juanbsanchez <juanbsanchez1988@gmail.com>
2021-06-20 10:24:00 +0200

    fill primetable with data

--- src/app/sales/pages/order/order.component.ts
+++ src/app/sales/pages/order/order.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import {Color, Heroe} from "../../interfaces/sales.interface";
 
 @Component({
   selector: 'app-order',
@@ -6,13 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class OrderComponent implements OnInit {
+export class OrderComponent {
+
   uppercase: boolean = false;
+  heroes: Heroe[] = [
+    {
+      name: 'Superman',
+      fly: true,
+      color: Color.blue
+    },
+    {
+      name: 'Batman',
+      fly: false,
+      color: Color.black
+    },
+    {
+      name: 'Robbin',
+      fly: false,
+      color: Color.green
+    },
+    {
+      name: 'Daredevil',
+      fly: false,
+      color: Color.red
+    },
+    {
+      name: 'Green Lantern',
+      fly: true,
+      color: Color.green
+    }
+  ]
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
   toUppercase(){
     this.uppercase = !this.uppercase;
